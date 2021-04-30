a67fd5b1f01a6a4b27d977ce71118ef9d2939887
desoto13 <mrmisa_windel@yahoo.com>
2021-04-30 21:38:09 +0800

    Ruby Assignment 2

--- assignment2_2.rb
+++ assignment2_2.rb
@@ -1,8 +1,8 @@
 print "Please input a number between 0 to 100:  "
 n = gets.chomp
-if n.to_f in 0..50
+if  (0..50) === n.to_f
     puts "#{n} is between 0 and 50"
-elsif n.to_f in 51..100
+elsif (51..100) === n.to_f
     puts "#{n} is between 51 and 100"
 elsif n.to_f > 100
     puts "#{n} is above 100"
