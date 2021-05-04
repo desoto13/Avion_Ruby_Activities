30b9751f0db67154797b9383c8d803a27b1fa95e
desoto13 <mrmisa_windel@yahoo.com>
2021-05-04 21:02:04 +0800

    Revision smallest integer

--- smallest_int.rb
+++ smallest_int.rb
@@ -1,21 +1,16 @@
 arr_one = [34,15,18,2]
 arr_two = [34,-345,-1,100]
-n = arr_one[0]
 
-# for i in 0..arr_one.length-1
-#     if arr_one[i].to_i < arr_one[i+1].to_i && i+1 != arr_one.length-1
-#         n = arr_one[i]
-#     else
-#         n = arr_one[i+1]
-#     end
-# end
-
-arr_one.each_with_index do |i,j|
-    if arr_one[i].to_i < arr_one[j].to_i && j != arr_one.length
-        n = arr_one[i]
-    else
-        n = arr_one[j]
+def smallest(arr)
+    n = arr[0]
+    arr.each do |number|
+        if number < n
+            n = number
+        end
     end
+    puts n
 end
 
-puts n
+smallest(arr_one)
+smallest(arr_two)
+
